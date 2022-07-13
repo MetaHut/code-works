25aef031f1a31f1dbead73d0a8f8be6ef1c6425c
zhuangchong <zhuangchong6@163.com>
2022-07-13 18:17:39 +0800

    update docs baseUrl

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
   url: 'https://metahut.github.io',
-  baseUrl: '/code',
+  baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
